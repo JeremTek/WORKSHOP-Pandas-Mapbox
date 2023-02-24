9a7fe3ecc79e0a1f8ace74d0b1bf1c9bd0f84020
Jeremy Roux <jeremy.roux@epitech.eu>
2023-02-24 12:19:10 +0100

    [ADD] the map now has color for the different departments. Nice

--- mapclean.py
+++ mapclean.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv('liste-des-gares.csv', sep=';', usecols=['commune', 'departemen', 'geo_point_2d'])
-df = df.dropna(subset=['commune', 'departemen', 'geo_point_2d'])
+df = pd.read_csv('liste-des-gares.csv', sep=';', usecols=['commune', 'departement', 'geo_point_2d'])
+df = df.dropna(subset=['commune', 'departement', 'geo_point_2d'])
 
 df[['latitude', 'longitude']] = df['geo_point_2d'].str.split(',', expand=True)
 df = df.drop('geo_point_2d', axis=1)
